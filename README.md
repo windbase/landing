d2aafb866cf8d546d8754e0ddbe7b95cf2ad6af3
mxvsh <xencodes@icloud.com>
2025-07-09 00:16:29 +0530

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Windbase Landing Page
+
+This is the landing page for [Windbase](https://windbase.dev). It is built with Astro and TailwindCSS.
+
+## Credits
+
+- [Web3Templates](https://web3templates.com)
---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://windbase.dev",
+  integrations: [tailwind(), mdx(), sitemap(), icon()],
+});
